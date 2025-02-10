defb72f945d5414a1e04fd1fbef0aa62ea06f904
piyush52dixit <piyushdixit567@gmail.com>
2025-02-10 22:15:49 +0530

    creating_databases_and_tables section completed.

---
+++ creating_databases_and_tables/createTable.sql
@@ -0,0 +1,18 @@
+CREATE TABLE <NAME_OF_TABLE> (
+    <column_name> Datatype,
+    <another_column_name> Datatype,
+    .
+    .
+    .
+);
+
+CREATE TABLE cats (
+    name VARCHAR(50),
+    age INT
+);
+ 
+CREATE TABLE dogs (
+    name VARCHAR(50),
+    breed VARCHAR(50),
+    age INT
+);
---
+++ creating_databases_and_tables/descTables.sql
@@ -0,0 +1,6 @@
+DESC <TABLE_NAME>;
+
+-- OR can do same thing with 
+
+SHOW COLUMNS FROM <TABLE_NAME>;
+
